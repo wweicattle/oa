43c6192b9c9ff563b1c83e0f960e2e26ae9443bd
xiaowei <996196542@qq.com>
2020-04-29 15:32:55 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,27 @@
-module.exports={
-    devServer: {
-        proxy: {
-          '/api': {
-            target: 'http://oa.wuwei.kooboo.site'
-          }
-        }
-      },
-    configureWebpack:{
-        resolve:{
-            alias:{
-                "assets":"@/assets",
-                "common":"@/common",
-                "network":"@/network",
-                "components":"@/components",
-                "views":"@/views",
-            }
-        }
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://oa.wuwei.kooboo.site'
+      }
     }
+  },
+  css: {
+    sourceMap: true
+  },
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("./src"))
+    config.plugin("html").tap(options => {
+      options[0].title = "OA";
+      return options;
+    });
+  }
 }
